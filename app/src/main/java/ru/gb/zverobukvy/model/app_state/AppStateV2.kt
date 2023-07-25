a2386cbd122bf1447e52c8ec4d80189155381f7b
alecsey_efimov <vezenye44@yandex.ru>
2023-07-25 21:16:50 +0300

    Финальные изменения в AppState 25.07.23

--- app/src/main/java/ru/gb/zverobukvy/model/app_state/AppStateV2.kt
+++ app/src/main/java/ru/gb/zverobukvy/model/app_state/AppStateV2.kt
@@ -24,105 +24,105 @@ sealed class Player(
     ) : Player(name, gameScore, color)
 }
 
-sealed interface State {
 
-    /** Супер состояние экрана
+/** Супер состояние экрана
+ */
+sealed interface AnimalLettersEntireState {
+
+    /** Состояние загрузки/подготовки игры.
+     * Дает интерактору время на загрузку карточек из репозитория.
      */
-    sealed interface SupperState : State {
+    object LoadingGameState : AnimalLettersEntireState
 
-        /** Состояние загрузки/подготовки игры.
-         * Дает интерактору время на загрузку карточек из репозитория.
-         */
-        object LoadingGameState : SupperState
-
-        /** Полное состояние экрана, передается в случаях:
-         * 1. Начла игры
-         * 2. При пересоздании View
-         *
-         * @param lettersCards Список всех карточек с буквами
-         * (карточка сама содержит информацию о том, какой стороной она повернута)
-         *
-         * @param wordCard Слово для отгадывания
-         * @param scores Текущий счет
-         * @param nextWalkingPlayer Ходящий игрок
-         * @param positionLetterInWord Все подсвечиваемые буквы
-         */
-        data class StartGameState(
-            val lettersCards: List<LetterCard>,
-            val wordCard: WordCard,
-            val scores: List<Player>,
-            val nextWalkingPlayer: Player,
-            val positionLetterInWord: List<Int>,
-        ) : SupperState
-
-        /** Состояние запроса на прекращение игры, показ диалогового окна
-         * (исходит из ViewModel)
-         */
-        object IsEndGameState : SupperState
-
-        /** Отмена состояния [IsEndGameState], закрытие диалогового окна о закрытии игры
-         * (исходит из ViewModel)
-         */
-        object IsLoadGameState : SupperState
-
-        /** Состояние окончания игры
-         * (исходит из Interactor)
-         *
-         * @param scores Итоговый счет
-         */
-        data class EndGameState(
-            val scores: List<Player>,
-        ) : SupperState
-    }
-
-    /** Передает состояния частей экрана в процессе игры
+    /** Полное состояние экрана, передается в случаях:
+     * 1. Начла игры
+     * 2. При пересоздании View
+     *
+     * @param lettersCards Список всех карточек с буквами
+     * (карточка сама содержит информацию о том, какой стороной она повернута)
+     *
+     * @param wordCard Слово для отгадывания
+     * @param scores Текущий счет
+     * @param nextWalkingPlayer Ходящий игрок
      */
-    sealed class PlayerWayState : State {
+    data class StartGameState(
+        val lettersCards: List<LetterCard>,
+        val wordCard: WordCard,
+        val scores: List<Player>,
+        val nextWalkingPlayer: Player,
+    ) : AnimalLettersEntireState
 
-        /** Состояние верно отгаданной карточки с буквой
-         *
-         * @param flippedLetterCard Карточка которую нужно перевернуть
-         * @param positionLetterInWord Отгаданная буква которую нужно подсветить
-         */
-        data class CorrectLetter(
-            val flippedLetterCard: LetterCard,
-            val positionLetterInWord: Int,
-        ) : PlayerWayState()
+    /** Состояние запроса на прекращение игры, показ диалогового окна
+     * (исходит из ViewModel)
+     */
+    object IsEndGameState : AnimalLettersEntireState
 
-        /** Состояния неверно выбранной карточки
-         *
-         * Действия :
-         * 1. Перевернуть выбранную карточку рубашкой вниз
-         * 2. Сообщить пользователю что карточка неверная
-         * 3. Перевернуть карточку обратно
-         *
-         * @param nextWalkingPlayer Следующего ходящего игрока
-         * @param invalidLetterCard Невалидную карточку
-         */
-        data class InvalidLetter(
-            val nextWalkingPlayer: Player,
-            val invalidLetterCard: LetterCard,
-        ) : PlayerWayState()
+    /** Состояние окончания игры
+     * (исходит из Interactor)
+     *
+     * @param scores Итоговый счет
+     */
+    data class EndGameState(
+        val scores: List<Player>,
+    ) : AnimalLettersEntireState
+}
 
-        /** Состояние успешно отгаданного слова.
-         * Посылается после отправки [CorrectLetter] (?с задержкой?)
-         *
-         * Подразумевает :
-         * 1. Смену загадываемого слова
-         * 2. Пререворачивание всех ранее открытых карточек рубашаками вверх
-         * 3. Очищение подсветки букв в загаданном слове
-         * 4. Смену игрока
-         *
-         * @param positionLetterInWord Позиция отгаданной буквы в слове
-         * @param wordCard Новое загаданное слово
-         * @param nextWalkingPlayer Игрок к которому переходит ход
-         * @param scores Обновленный счет игроков
-         */
-        data class GuessedWord(
-            val positionLetterInWord: Int,
-            val wordCard: WordCard?,
-            val nextWalkingPlayer: Player?,
-            val scores: List<Player>,
-        ) : PlayerWayState()
-    }
+/** Передает состояния частей экрана в процессе игры
+ */
+sealed class AnimalLettersChangingState {
+
+    /** Состояние верно отгаданной карточки с буквой
+     *
+     * @param correctLetterCard Карточка которую нужно перевернуть
+     * @param positionLetterInWord Отгаданная буква которую нужно подсветить
+     */
+    data class CorrectLetter(
+        val correctLetterCard: LetterCard,
+        val positionLetterInWord: Int,
+    ) : AnimalLettersChangingState()
+
+    /** Состояния неверно выбранной карточки
+     *
+     * Действия :
+     * 1. Перевернуть выбранную карточку рубашкой вниз
+     * 2. Сообщить пользователю что карточка неверная
+     *
+     * @param invalidLetterCard Невалидную карточку
+     */
+    data class InvalidLetter(
+        val invalidLetterCard: LetterCard,
+    ) : AnimalLettersChangingState()
+
+    /** Передача хода следующему игроку.
+     * Вызывается после [InvalidLetter]
+     *
+     * Действия :
+     * 1. Перевернуть карточку обратно
+     * 2. Передать ход следующему игроку
+     *
+     * @param nextWalkingPlayer Следующего ходящего игрока
+     * @param invalidLetterCard Невалидную карточку
+     */
+    data class NextPlayer(
+        val nextWalkingPlayer: Player,
+        val invalidLetterCard: LetterCard,
+    ) : AnimalLettersChangingState()
+
+    /** Состояние успешно отгаданного слова.
+     * Посылается после отправки [CorrectLetter] (?с задержкой?)
+     *
+     * Подразумевает :
+     * 1. Пререворачивание всех ранее открытых карточек рубашаками вверх
+     * 2. Очищение подсветки букв в загаданном слове
+     * 3. Смену игрока
+     *
+     * @param wordCard Новое загаданное слово
+     * @param nextWalkingPlayer Игрок к которому переходит ход
+     * @param players Обновленный счет игроков
+     */
+    data class GuessedWord(
+        val wordCard: WordCard?,
+        val nextWalkingPlayer: Player?,
+        val players: List<Player>,
+    ) : AnimalLettersChangingState()
 }
