2daf5297317ec95c630b0d40ae7c9195792337a6
egorovin <egorovin@mail.ru>
2023-07-27 03:13:25 +0300

    test custom view

--- app/src/main/java/ru/gb/zverobukvy/TestCustomCardTableFragment.kt
+++ app/src/main/java/ru/gb/zverobukvy/TestCustomCardTableFragment.kt
@@ -1,10 +1,13 @@
 package ru.gb.zverobukvy
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.gb.zverobukvy.databinding.FragmentTestCustomCardTableBinding
+import ru.gb.zverobukvy.presentation.customview.CustomCard
+import ru.gb.zverobukvy.presentation.customview.CustomCardTable
 
 class TestCustomCardTableFragment : Fragment() {
 
@@ -15,9 +18,56 @@ class TestCustomCardTableFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test_custom_card_table, container, false)
+    ): View {
+        val binding = FragmentTestCustomCardTableBinding.inflate(inflater, container, false)
+        initUI(binding)
+        return binding.root
+    }
+
+    private fun initUI(binding: FragmentTestCustomCardTableBinding) {
+        binding.setListButton.setOnClickListener {
+            val letterCardList = listOf(
+                CustomCardTable.LetterCard('A', false, "img1.png"),
+                CustomCardTable.LetterCard('B', false, "img2.png"),
+                CustomCardTable.LetterCard('C', false, "img3.png"),
+                CustomCardTable.LetterCard('D', true, "img4.png"),
+                CustomCardTable.LetterCard('E', false, "img5.png"),
+                CustomCardTable.LetterCard('F', false, "img6.png"),
+                CustomCardTable.LetterCard('A', true, "img7.png"),
+                CustomCardTable.LetterCard('A', true, "img8.png"),
+                CustomCardTable.LetterCard('A', false, "img9.png"),
+                //                CustomCardTable.LetterCard('A', false, "img10.png"),
+                //                CustomCardTable.LetterCard('C', false, "img3.png"),
+                //                CustomCardTable.LetterCard('D', true, "img4.png"),
+                //                CustomCardTable.LetterCard('E', false, "img5.png"),
+                //                CustomCardTable.LetterCard('F', false, "img6.png"),
+                //                CustomCardTable.LetterCard('A', true, "img7.png"),
+                //                CustomCardTable.LetterCard('A', true, "img8.png"),
+                //                CustomCardTable.LetterCard('A', false, "img9.png"),
+                //                CustomCardTable.LetterCard('A', false, "img10.png"),
+                //                CustomCardTable.LetterCard('E', false, "img5.png"),
+                //                CustomCardTable.LetterCard('F', false, "img6.png"),
+                //                CustomCardTable.LetterCard('A', true, "img7.png"),
+                //                CustomCardTable.LetterCard('A', true, "img8.png"),
+                //                CustomCardTable.LetterCard('A', false, "img9.png"),
+                //                CustomCardTable.LetterCard('A', false, "img10.png"),
+                //                CustomCardTable.LetterCard('C', false, "img3.png"),
+                //                CustomCardTable.LetterCard('D', true, "img4.png"),
+                //                CustomCardTable.LetterCard('E', false, "img5.png"),
+                //                CustomCardTable.LetterCard('F', false, "img6.png"),
+                //                CustomCardTable.LetterCard('A', true, "img7.png"),
+                //                CustomCardTable.LetterCard('A', true, "img8.png"),
+                //                CustomCardTable.LetterCard('A', false, "img9.png"),
+                //                CustomCardTable.LetterCard('A', false, "img10.png"),
+                //                CustomCardTable.LetterCard('A', false, "img8.png")
+            )
+            binding.cardTable.setListItem(letterCardList, "img1.png") {
+                CustomCard(requireContext()).apply {
+                    radius = 50f
+                    elevation = 3f
+                }
+            }
+        }
     }
 
     companion object {
