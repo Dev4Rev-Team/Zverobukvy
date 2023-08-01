194534972ae2aed8ee2b4bb354010f0b1601a5ec
egorovin <egorovin@mail.ru>
2023-08-02 02:21:07 +0300

    in LetterCard add implement CustomCardTable.LetterCardUI

--- app/src/main/java/ru/gb/zverobukvy/domain/entity/LetterCard.kt
+++ app/src/main/java/ru/gb/zverobukvy/domain/entity/LetterCard.kt
@@ -1,12 +1,14 @@
 package ru.gb.zverobukvy.domain.entity
 
+import ru.gb.zverobukvy.presentation.customview.CustomCardTable
+
 data class LetterCard(
-    val letter: Char,
+    override val letter: Char,
     override val typesCards: List<TypeCards>,
-    var isVisible: Boolean = false,
-    val faceImageName: String,
-    val backImageName: String = BACK_IMAGE_NAME
-) : Card {
+    override var isVisible: Boolean = false,
+    override val faceImageName: String,
+    override val backImageName: String = BACK_IMAGE_NAME
+) : Card, CustomCardTable.LetterCardUI {
     companion object {
         const val BACK_IMAGE_NAME = "BACK_IMAGE.jpg"
     }
