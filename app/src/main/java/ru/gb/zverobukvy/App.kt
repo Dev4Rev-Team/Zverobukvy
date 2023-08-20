b82b22453dbfcbb7ba47d180ecbb0f3eeab8a7ce
egorovin <egorovin@mail.ru>
2023-08-20 04:58:48 +0300

    create animalLettersCardsRepository in App add fragment on start invisible

--- app/src/main/java/ru/gb/zverobukvy/App.kt
+++ app/src/main/java/ru/gb/zverobukvy/App.kt
@@ -1,10 +1,17 @@
 package ru.gb.zverobukvy
 
 import android.app.Application
+import ru.gb.zverobukvy.data.data_source_impl.LocalDataSourceImpl
+import ru.gb.zverobukvy.data.mapper.LetterCardsMapper
+import ru.gb.zverobukvy.data.mapper.WordCardsMapper
+import ru.gb.zverobukvy.data.repository_impl.AnimalLettersCardsRepositoryImpl
 import ru.gb.zverobukvy.data.room.AnimalLettersDatabase
+import ru.gb.zverobukvy.domain.repository.AnimalLettersCardsRepository
 import timber.log.Timber
 
 class App : Application() {
+    lateinit var animalLettersCardsRepository: AnimalLettersCardsRepository
+
     override fun onCreate() {
         super.onCreate()
         // устанавливаем DebugTree() для Timber
@@ -13,5 +20,10 @@ class App : Application() {
         }
         // создаем AnimalLettersDatabase
         AnimalLettersDatabase.createInstanceDatabase(this)
+        animalLettersCardsRepository = AnimalLettersCardsRepositoryImpl(
+            LocalDataSourceImpl(AnimalLettersDatabase.getPlayersDatabase()),
+            LetterCardsMapper(),
+            WordCardsMapper()
+        )
     }
 }
