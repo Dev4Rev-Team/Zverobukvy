2702fa3c981b8f56ebdfd701f55bf55f3f98d601
egorovin <egorovin@mail.ru>
2023-09-03 02:20:07 +0300

    animation invalid/valid card

--- app/src/main/java/ru/gb/zverobukvy/presentation/customview/CustomAnimation.kt
+++ app/src/main/java/ru/gb/zverobukvy/presentation/customview/CustomAnimation.kt
@@ -5,15 +5,26 @@ import android.animation.ObjectAnimator
 import android.view.View
 import androidx.cardview.widget.CardView
 import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
+
+private const val OPEN_BEGIN = 0f
+private const val CLOSE_BEGIN = 90f
+private const val CLOSE_END = 270f
+private const val OPEN_END = 360f
+private const val INVISIBLE = 0f
+private const val VISIBLE = 1f
+
 
 fun createFlipAnimation(view: View, changeFace: (() -> Unit)? = null): AnimatorSet {
     val animatorSet = AnimatorSet()
-    val rotationClosing = ObjectAnimator.ofFloat(view, CardView.ROTATION_Y, 0f, 90f).apply {
-        doOnEnd {
-            changeFace?.invoke()
+
+    val rotationClosing =
+        ObjectAnimator.ofFloat(view, CardView.ROTATION_Y, OPEN_BEGIN, CLOSE_BEGIN).apply {
+            doOnEnd {
+                changeFace?.invoke()
+            }
         }
-    }
-    val rotationOpening = ObjectAnimator.ofFloat(view, CardView.ROTATION_Y, 270f, 360f)
+    val rotationOpening = ObjectAnimator.ofFloat(view, CardView.ROTATION_Y, CLOSE_END, OPEN_END)
     animatorSet.playSequentially(rotationClosing, rotationOpening)
     return animatorSet
 }
@@ -24,4 +35,14 @@ fun createScaleAnimation(view: View, x1: Float, x2: Float): AnimatorSet {
     val scaleY = ObjectAnimator.ofFloat(view, CardView.SCALE_Y, x1, x2)
     animatorSet.playTogether(scaleX, scaleY)
     return animatorSet
+}
+
+fun createAlphaShowAnimation(view: View, startDelay: Long, duration: Long): ObjectAnimator {
+    return ObjectAnimator.ofFloat(view, View.ALPHA, INVISIBLE, VISIBLE).also {
+        it.startDelay = startDelay
+        it.duration = duration
+        it.doOnStart {
+            view.visibility = View.VISIBLE
+        }
+    }
 }
