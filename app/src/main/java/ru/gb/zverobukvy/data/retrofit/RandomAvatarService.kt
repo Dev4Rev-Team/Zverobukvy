67880c7f097c7ba9d71337895228fb3b6a2d0a3c
alecsey_efimov <vezenye44@yandex.ru>
2023-09-04 10:11:31 +0300

    ?? fix запроса к Api

--- app/src/main/java/ru/gb/zverobukvy/data/retrofit/RandomAvatarService.kt
+++ app/src/main/java/ru/gb/zverobukvy/data/retrofit/RandomAvatarService.kt
@@ -1,10 +1,10 @@
 package ru.gb.zverobukvy.data.retrofit
 
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RandomAvatarService {
 
-    @GET("adventurer/jpg?seed={seed}")
-    suspend fun getSvgImageEntityBySeed(@Path("seed") seed: String): AvatarApi
+    @GET("adventurer/jpg")
+    suspend fun getSvgImageEntityBySeed(@Query("seed") seed: String): AvatarApi
 }
