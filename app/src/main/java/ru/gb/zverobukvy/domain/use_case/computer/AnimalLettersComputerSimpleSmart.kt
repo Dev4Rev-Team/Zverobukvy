9b99fbe8fd692a0626ecf2ad9edcc2586f1230a6
egorovin <egorovin@mail.ru>
2023-09-22 02:42:33 +0300

    AnimalLettersComputerSimpleSmart

---
+++ app/src/main/java/ru/gb/zverobukvy/domain/use_case/computer/AnimalLettersComputerSimpleSmart.kt
@@ -0,0 +1,59 @@
+package ru.gb.zverobukvy.domain.use_case.computer
+
+import ru.gb.zverobukvy.domain.entity.GameField
+import timber.log.Timber
+
+/**
+ * @param smartLevel
+ * [0,1] - вероятность правильного хода
+ */
+
+class AnimalLettersComputerSimpleSmart(
+    private val probabilityIsCorrect: Float,
+    private var gameField: GameField,
+) : AnimalLettersComputer {
+    private val lettersInvisible: MutableSet<Int> = mutableSetOf()
+    private val lettersRemember: MutableList<Int> = mutableListOf()
+    private val lettersForGame: MutableSet<Int> = mutableSetOf()
+
+    override fun setCurrentGameField(currentGameField: GameField, positionLastSelectionLetterCard: Int) {
+        Timber.d("setCurrentGameField, position $positionLastSelectionLetterCard")
+        updateLettersInvisible(currentGameField)
+        updateLettersRemember(positionLastSelectionLetterCard)
+    }
+
+    override fun getSelectedLetterCard(): Int {
+        Timber.d("getSelectedLetterCard")
+        updateLettersForGame()
+        return lettersForGame.random()
+    }
+
+    private fun updateLettersForGame() {
+        lettersForGame.clear()
+        lettersForGame.addAll(lettersInvisible)
+        lettersForGame.removeAll(lettersRemember.toSet())
+    }
+
+    private fun updateLettersRemember(selectedPosition: Int) {
+        if (selectedPosition >= 0) {
+            lettersRemember.add(selectedPosition)
+        }
+        if (lettersRemember.size >= MAX_REMEMBER) {
+            lettersRemember.removeLast()
+        }
+    }
+
+    private fun updateLettersInvisible(currentGameField: GameField) {
+        lettersInvisible.clear()
+        currentGameField.lettersField.forEachIndexed { index, letterCard ->
+            if (!letterCard.isVisible) {
+                lettersInvisible.add(index)
+            }
+        }
+    }
+
+    companion object {
+        const val MAX_REMEMBER = 6
+    }
+
+}
