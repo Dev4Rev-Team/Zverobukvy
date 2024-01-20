0e85dae3797f2bf16f393db516866bc866dac626
Egorovin <girogevoro@gmail.com>
2024-01-20 18:20:43 +0300

    fix

--- app/src/main/java/ru/dev4rev/kids/zoobukvy/utility/Utils.kt
+++ app/src/main/java/ru/dev4rev/kids/zoobukvy/utility/Utils.kt
@@ -3,15 +3,8 @@ package ru.dev4rev.kids.zoobukvy.utility
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Parcelable
-import ru.dev4rev.kids.zoobukvy.configuration.Conf
 
 inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
     SDK_INT >= 33 -> getParcelable(key, T::class.java)
     else -> @Suppress("DEPRECATION") getParcelable(key) as? T
-}
-
-inline fun isDebugRun(flag: Boolean, block: () -> Unit) {
-    if (Conf.DEBUG && flag) {
-        block.invoke()
-    }
 }
