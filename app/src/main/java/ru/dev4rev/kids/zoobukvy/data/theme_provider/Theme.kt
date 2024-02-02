aa4b64ff8a60fa1ddfdbc0df6619c5366fa7d9df
Barekaev Dmitry <barekaev@bk.ru>
2024-02-03 00:44:20 +0300

    Добавлены вариации тем по временам года

--- app/src/main/java/ru/dev4rev/kids/zoobukvy/data/theme_provider/Theme.kt
+++ app/src/main/java/ru/dev4rev/kids/zoobukvy/data/theme_provider/Theme.kt
@@ -3,5 +3,9 @@ package ru.dev4rev.kids.zoobukvy.data.theme_provider
 import ru.dev4rev.kids.zoobukvy.R
 
 enum class Theme(val idTheme: Int) {
-    BASE(R.style.Theme_Zverobukvy_Winter), NEW_YEAR(R.style.Theme_Zverobukvy_NewYear)
+    BASE(R.style.Base_Theme_Zverobukvy),
+    WINTER(R.style.Theme_Zverobukvy_Winter), NEW_YEAR(R.style.Theme_Zverobukvy_NewYear),
+    SPRING(R.style.Theme_Zverobukvy_Spring),
+    SUMMER(R.style.Theme_Zverobukvy_Spring),
+    AUTUMN(R.style.Theme_Zverobukvy_Autumn)
 }
