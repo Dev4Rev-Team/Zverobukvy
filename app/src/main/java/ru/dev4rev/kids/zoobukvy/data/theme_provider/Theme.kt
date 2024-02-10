50ec430e187de85399ae9650568c40751a13c498
Barekaev Dmitry <barekaev@bk.ru>
2024-02-10 16:25:58 +0300

    Изменения в дизайне: - Добавлена летняя тема; - В осенней теме добавлен персонаж; - Изменена рубашка карточек в зимней теме; - Иконки звука заменены на webp; - Улучшение качества изображений с персонажами; - Заменены изображения в инструкции;

--- app/src/main/java/ru/dev4rev/kids/zoobukvy/data/theme_provider/Theme.kt
+++ app/src/main/java/ru/dev4rev/kids/zoobukvy/data/theme_provider/Theme.kt
@@ -7,6 +7,6 @@ enum class Theme(val themeId: Int) {
     WINTER(R.style.Theme_Zverobukvy_Winter),
     NEW_YEAR(R.style.Theme_Zverobukvy_NewYear),
     SPRING(R.style.Theme_Zverobukvy_Spring),
-    SUMMER(R.style.Theme_Zverobukvy_Spring),
+    SUMMER(R.style.Theme_Zverobukvy_Summer),
     AUTUMN(R.style.Theme_Zverobukvy_Autumn)
 }
